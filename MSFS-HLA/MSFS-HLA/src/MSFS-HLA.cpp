29bd75837a65af4833e70495343ee9016ca70493
JP-Souto <jpsfernandes2014@gmail.com>
2025-01-10 20:04:29 -0300

    Adiciona mais arquivos iniciais

---
+++ MSFS-HLA/MSFS-HLA/src/MSFS-HLA.cpp
@@ -0,0 +1,16 @@
+#include <RTI/RTI1516.h>
+#include <RTI/RTIambassadorFactory.h>
+#include <iostream>
+
+int main() {
+    try {
+        rti1516e::RTIambassadorFactory factory;
+        auto rtiAmbassador = factory.createRTIambassador();
+        std::wcout << L"RTI criado com sucesso!" << std::endl;
+    }
+    catch (const rti1516e::Exception& e) {
+        std::wcerr << L"Erro ao criar RTI: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ MSFS-HLA/MSFS-HLA/src/SimConnect.cpp
@@ -0,0 +1,27 @@
+#include <windows.h>
+#include <iostream>
+#include <SimConnect.h>
+/*
+SimConnect* simConnect = nullptr;
+HRESULT hr;
+
+int main()
+{
+    // Tente conectar ao Flight Simulator
+    hr = SimConnect_Open(&simConnect, "MSFS-HLA", nullptr, 0, 0, 0);
+
+    if (hr == S_OK) {
+        std::cout << "Conexão com o Flight Simulator bem-sucedida!" << std::endl;
+    }
+    else {
+        std::cout << "Falha na conexão com o Flight Simulator." << std::endl;
+    }
+
+    // Fechar a conexão com o Flight Simulator quando terminar
+    if (simConnect) {
+        SimConnect_Close(simConnect);
+    }
+
+    return 0;
+}
+*/
